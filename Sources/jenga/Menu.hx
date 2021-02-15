ea60d9c3599bde470c82a591178843fff5d9455f
tong <tong@disktree.net>
2021-02-15 21:59:26 +0100

    Show semver

--- Sources/jenga/Menu.hx
+++ Sources/jenga/Menu.hx
@@ -5,14 +5,16 @@ import zui.Themes;
 
 class Menu extends iron.Trait {
     
-    var game : Game;
+    var font : Font;
     var ui : Zui;
+    var game : Game;
 
     public function new() {
         super();
         notifyOnInit( () -> {
             game = Scene.active.getTrait( jenga.Game );
             Data.getFont( 'mono.ttf', font -> {
+                this.font = font;
                 ui = new Zui( { font : font } );
                 notifyOnUpdate( update );
                 notifyOnRender2D( render2D );
@@ -22,26 +24,38 @@ class Menu extends iron.Trait {
 
     function update() {
         var kb = iron.system.Input.getKeyboard();
-        if( kb.started( "escape" ) || kb.started( "space" ) ) {
+        if( kb.started( "escape" ) || kb.started( "space" ) || kb.started( "f5" ) ) {
             game.start();
         }
     }
 
     function render2D( g : kha.graphics2.Graphics ) {
+        
         if( ui == null ) return;
+        
         var sw = System.windowWidth(), sh = System.windowHeight();
+        
         g.end();
+
 		ui.begin( g );
 		//ui.beginRegion( g );
         g.opacity = 1;
         var hwin = Id.handle();
         hwin.redraws = 1;
-        if( ui.window( hwin, 1, 1, 200, 200, false ) ) {
+        if( ui.window( hwin, 1, 1, 120, 40, false ) ) {
             if( ui.button( "JENGA!", Left ) ) game.start();
             //if( ui.button( "CLEAR", Left ) ) game.clear();
-            if( ui.button( "CREATE", Left ) ) game.create(32);
+            //if( ui.button( "CREATE", Left ) ) game.create(32);
         }
         ui.end();
+
+        g.color = 0xff000000;
+		g.font = font;
+		g.fontSize = Std.int(12);
+        var text = 'v'+Main.projectVersion;
+        var textWidth = font.width( g.fontSize, text );
+        g.drawString( text, sw-(textWidth + (g.fontSize*1.1)), sh-(g.fontSize*1.5) ); 
+
         g.begin( false );
     }
 }
